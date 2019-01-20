2571e3ac69679aec265946535f6b9ab51fe18822
BengBu-YueZhang <zhangyue_coder@163.com>
2019-01-20 12:05:20 +0800

    添加list的组件

---
+++ vue-beyond/src/components/Cascader/index.vue
@@ -0,0 +1,37 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'Cascader',
+
+  props: {
+    isMultiple: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+
+    options: {
+      type: Array,
+      required: false,
+      default: false
+    },
+
+    value: {
+      type: Array,
+      required: false,
+      default: []
+    }
+  },
+
+  data () {
+    return {
+      cascaderOptions: []
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ vue-beyond/src/components/List/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="list__wrapper">
+    <ul class="list__list">
+      <li
+        v-for="(item, index) in options"
+        :key="index"
+        @click="handleListClick(item)"
+        :class="{'list__list-item-active': item === value}"
+      >
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+
+  props: {
+    // 是否显示多选
+    isMultiple: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+
+    // 是否显示勾选的icon
+    isIcon: {
+      type: Boolean,
+      required: false,
+      default: true
+    },
+
+    options: {
+      type: Array,
+      required: false,
+      default: false
+    },
+
+    value: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+
+  metation: {
+    handleListClick (item) {
+      this.$emit('click', item)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import url('../../styles/variable.less');
+
+.list__wrapper {
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+
+.list__list {
+  width: 100%;
+  height: auto;
+  background-color: @white;
+}
+
+.list__list-item {
+  font-size: 14px;
+  line-height: 30px;
+  color: @grey500;
+  text-align: center;
+}
+
+.list__list-item-active {
+  color: @green500;
+}
+</style>
