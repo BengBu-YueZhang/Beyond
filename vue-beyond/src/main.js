709fa645a45f6b5ef2bfc3905caabe6f3eb1ca66
BengBu-YueZhang <zhangyue_coder@163.com>
2019-01-20 11:21:42 +0800

    添加移动端的postcss

--- vue-beyond/src/main.js
+++ vue-beyond/src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/**
+ * 兼容低版本安坐，转换vw的单位
+ */
+var hacks = require('viewport-units-buggyfill/viewport-units-buggyfill.hacks')
+
+require('viewport-units-buggyfill').init({
+  hacks: hacks
+})
+
+
 Vue.config.productionTip = false
 
 new Vue({
