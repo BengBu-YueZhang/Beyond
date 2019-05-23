bf49db5e77a108bddc23cc99069e4cd79d071fb3
BengBu-YueZhang <zhangyue_coder@163.com>
2019-05-23 16:54:12 +0800

    new branch new start

---
+++ src/packages/Icon/icon.vue
@@ -0,0 +1,61 @@
+<template>
+  <i :style="styles" class="icons" @click="handleClick">
+    <slot></slot>
+  </i>
+</template>
+
+<script>
+export default {
+  name: "Icons",
+
+  props: {
+    size: {
+      type: [Number, String] 
+    },
+
+    color: {
+      type: String
+    }
+  },
+
+  computed: {
+    styles () {
+      let style = {}
+
+      if (this.size) {
+        style['font-size'] = `${this.size}px`
+      }
+
+      if (this.color) {
+        style['color'] = `${this.color}`
+      }
+
+      return style
+    }
+  },
+
+  methods: {
+    handleClick (event) {
+      this.$emit('click', event)
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.icons {
+  font-family: "Material Icons";
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;
+  line-height: 1;
+  letter-spacing: normal;
+  text-transform: none;
+  display: inline-block;
+  white-space: nowrap;
+  word-wrap: normal;
+  direction: ltr;
+  -webkit-font-feature-settings: "liga";
+  -webkit-font-smoothing: antialiased;
+}
+</style>
