d04ecd33d09fe14e17014a4168c9c2e215be6a38
BengBu-YueZhang <zhangyue_coder@163.com>
2019-05-23 14:59:54 +0800

    init

--- react-beyond/src/util/index.ts
+++
@@ -1,7 +0,0 @@
-export function isFunc (func: any): boolean {
-  return Object.prototype.toString.call(func) === '[object Function]'
-}
-
-export function isStr (str: any): boolean {
-  return Object.prototype.toString.call(str) === '[object String]'
-}
