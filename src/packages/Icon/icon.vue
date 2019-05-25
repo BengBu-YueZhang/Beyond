40fec0b9268919893599fb95b8b162b8bd29e277
BengBu-YueZhang <zhangyue_coder@163.com>
2019-05-25 21:03:07 +0800

    update css

--- src/packages/Icon/icon.vue
+++ src/packages/Icon/icon.vue
@@ -1,5 +1,5 @@
 <template>
-  <i :style="styles" class="icons" @click="handleClick">
+  <i :style="styles" class="dlz-icons" @click="handleClick">
     <slot></slot>
   </i>
 </template>
@@ -7,6 +7,8 @@
 <script lang="ts">
 import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
+
+const prefixClass = 'dlz-icon';
 
 interface Style {
   'font-size'?: string | number;
@@ -34,27 +36,3 @@ export default class Icon extends Vue {
   }
 }
 </script>
-
-<style lang="less" scoped>
-.icons {
-  font-family: 'Material Icons';
-  font-weight: normal;
-  font-style: normal;
-  font-size: 24px;  /* Preferred icon size */
-  display: inline-block;
-  line-height: 1;
-  text-transform: none;
-  letter-spacing: normal;
-  word-wrap: normal;
-  white-space: nowrap;
-  direction: ltr;
-  /* Support for all WebKit browsers. */
-  -webkit-font-smoothing: antialiased;
-  /* Support for Safari and Chrome. */
-  text-rendering: optimizeLegibility;
-  /* Support for Firefox. */
-  -moz-osx-font-smoothing: grayscale;
-  /* Support for IE. */
-  font-feature-settings: 'liga';
-}
-</style>
