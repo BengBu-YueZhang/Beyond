ebe90978ba17f7eda3211bd22473c5a8b2fddf0d
BengBu-YueZhang <zhangyue_coder@163.com>
2019-05-28 00:19:40 +0800

    add Input Component

---
+++ src/packages/Input/Input.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input ref="reference"/>
+    <transition>
+      <div ref="popper"></div>
+    </transition>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import Pop from '../../lib/popper';
+
+@Component({
+  name: 'Input',
+})
+export default class Input extends Vue {
+  private reference!: Element;
+
+  private popper!: Pop;
+
+  private mounted(): void {
+    this.$nextTick(() => {
+      this.popper = new Pop(
+        this.$refs.reference as Element,
+        this.$refs.popper as Element,
+      );
+    });
+  }
+}
+</script>
---
+++ src/views/input.view.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <Input/>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import Input from '../packages/Input';
+
+@Component({
+  components: {
+    Input,
+  },
+})
+export default class InputView extends Vue {
+}
+</script>
+
+<style lang="less" scoped>
+</style>
