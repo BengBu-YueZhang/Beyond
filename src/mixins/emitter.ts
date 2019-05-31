35a2aefd99abf1641f30a09dcd0ceaa857850baa
BengBu-YueZhang <zhangyue_coder@163.com>
2019-05-31 14:43:43 +0800

    add broadcast

--- src/mixins/emitter.ts
+++ src/mixins/emitter.ts
@@ -1,13 +1,23 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
-import { findParentComponent } from '../utils/find';
+import { findParentComponent, findChildsComponent } from '../utils/find';
 
 @Component
 export class EmitterMixin extends Vue {
+
   private dispatch(targetName: string, eventName: string, params: any): void {
     const parent = findParentComponent(this, targetName);
     if (parent) {
       parent.$emit.apply(parent, [eventName, params]);
+    }
+  }
+
+  private broadcast(targetName: string, eventName: string, params: any): void {
+    const chilrens = findChildsComponent(this, targetName);
+    if (chilrens && chilrens.length) {
+      chilrens.forEach((child) => {
+        child.$emit.apply(child, [eventName, params]);
+      });
     }
   }
 }
