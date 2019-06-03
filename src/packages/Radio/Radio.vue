ebb5951e29dd4cf90ee65044cb6619cbf9f58d4f
DavidDai <185098535@qq.com>
2019-06-03 19:27:42 +0800

    add radio

---
+++ src/packages/Radio/Radio.vue
@@ -0,0 +1,19 @@
+<template>
+  <label :class="mainClass">
+    <span :class="radioClass">
+      <span :class="innerClass"></span>
+      <input type="radio"/>
+    </span>
+    <slot>{{ label }}</slot>
+  </label>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+import { Component, Prop } from 'vue-property-decorator';
+const prefixClass = 'dlz-radio';
+
+@Component
+export default class Radio extends Vue {
+  
+}
+</script>
