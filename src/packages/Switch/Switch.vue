0f22c52b4861f49a0314849d9f1fa2900f590841
BengBu-YueZhang <zhangyue_coder@163.com>
2019-06-04 16:02:30 +0800

    update switch component

--- src/packages/Switch/Switch.vue
+++ src/packages/Switch/Switch.vue
@@ -1,5 +1,8 @@
 <template>
-  <span @click="handleClick" :class="switchClasses">
+  <span
+    @click="handleClick"
+    :class="switchClasses"
+    :style="switchStyles">
   </span>
 </template>
 
@@ -10,13 +13,21 @@ import { Component, Prop, Watch } from 'vue-property-decorator';
 const prefixClass = 'dlz-switch';
 
 @Component({
-  name: 'Switch',
+  name: 'NirvanaSwitch',
+  model: {
+    prop: 'value',
+    event: 'change',
+  },
 })
 export default class Switch extends Vue {
   @Prop({ default: false }) private value!: boolean;
   @Prop({ default: '' }) private activeClass!: string;
   @Prop({ default: '' }) private inactiveClass!: string;
+  @Prop({ default: '' }) private disabledActiveClass!: string;
+  @Prop({ default: '' }) private disabledInactiveClass!: string;
   @Prop({ default: false }) private disabled!: boolean;
+  @Prop({ default: '' }) private activeColor!: string;
+  @Prop({ default: '' }) private inactiveColor!: string;
 
   private currentValue: boolean = this.value;
 
@@ -31,8 +42,24 @@ export default class Switch extends Vue {
       [`${prefixClass}-disabled`]: this.disabled,
       [`${prefixClass}-active`]: this.currentValue,
       [`${prefixClass}-inactive`]: !this.currentValue,
+      [`${this.activeClass}`]: this.currentValue,
+      [`${this.inactiveClass}`]: !this.currentValue,
+      [`${this.disabledActiveClass}`]: this.disabled && this.currentValue,
+      [`${this.disabledInactiveClass}`]: this.disabled && !this.currentValue,
     };
     return switchClass;
+  }
+
+  get switchStyles(): object {
+    const switchStyle: any = {};
+    if (this.currentValue && this.activeColor) {
+      switchStyle.backgroundColor = this.activeColor;
+      switchStyle.borderColor = this.activeColor;
+    } else if (!this.currentValue && this.inactiveColor) {
+      switchStyle.backgroundColor = this.inactiveColor;
+      switchStyle.borderColor = this.inactiveColor;
+    }
+    return switchStyle;
   }
 
   private handleClick(): void {
